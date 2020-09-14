6871101cc58627fb20c374def37f877499b8245a
Victor Govea <victoregovea@gmail.com>
2020-09-14 16:32:10 -0400

    First Section Completed

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var options = {
+    strings: ["Beautiful","Responsive","User Friendly"],
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 75,
+    backDelay: 2000,
+    startDelay: 1000,
+  }
+  
+  var typed = new Typed(".typed", options);
